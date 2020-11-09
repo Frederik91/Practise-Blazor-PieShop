3193c142f4c6c7d94c115521ec775624aaa79aeb
Frederik Tegnander <frederik.t91@gmail.com>
2020-11-09 21:59:33 +0100

    Start components practise

--- PieShop.App/Pages/EmployeeDetail.cs
+++ PieShop.App/Pages/EmployeeDetail.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Components;
 using PieShop.App.Services;
+using PieShop.ComponentsLibrary.Map;
 using PieShop.Shared;
 
 namespace PieShop.App.Pages
@@ -16,9 +17,21 @@ namespace PieShop.App.Pages
 
         [Inject] private IEmployeeDataService EmployeeDataService { get; set; }
 
+        public List<Marker> MapMarkers { get; set; } = new List<Marker>();
+
         protected override async Task OnInitializedAsync()
         {
             Employee = await EmployeeDataService.GetEmployeeDetails(EmployeeId);
+            MapMarkers = new List<Marker>
+            {
+                new Marker
+                {
+                    X = Employee.Longitude, 
+                    Y = Employee.Latitude,
+                    Description = Employee.FirstName + " " + Employee.LastName,
+                    ShowPopup = false
+                }
+            };
         }
     }
 }
---
+++ PieShop.ComponentsLibrary/ProfilePictureBase.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.AspNetCore.Components;
+
+namespace PieShop.ComponentsLibrary
+{
+    public class ProfilePictureBase : ComponentBase
+    {
+        protected string CssClass { get; set; } = "circle";
+
+        [Parameter] public RenderFragment ChildContent { get; set; }
+
+        protected void PictureClick()
+        {
+            CssClass = CssClass == "circle" ? null : "circle";
+        }
+    }
+}
